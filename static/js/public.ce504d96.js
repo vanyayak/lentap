8252e350c76776750f784eb127acf84cebaaad51
vanyayak <vanya-yaki@yandex.ru>
2025-04-03 18:11:52 +0300

    Add files via upload

---
+++ static/js/public.ce504d96.js
@@ -0,0 +1,12 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[624],{87012:()=>{var o="stub_footer__F+wcZ",s="stub_loader__DQ46F",l="stub_logo__8OWhs",a="stub_main__AOEQp",e="stub_stub__dYrTC",d;window.moveTo(0,0),window.resizeTo(screen.availWidth,screen.availHeight);const t=document.getElementById("root");(t==null?void 0:t.innerHTML)===""&&(t.innerHTML=`
+<div class="${e}">
+  <div class="${a}">
+    
+    <img alt="logo" class="${l}" src="${((d=window.defaultData)==null?void 0:d.logo)||window.PLATFORM.logoFull_M}" />
+    <div class="${s}"></div>
+  </div>
+  <div class="footer">
+    ${window.PLATFORM.courseHtmlFooter}
+  </div>
+</div>
+`)}},o=>{var s=a=>o(o.s=a);o.O(0,[646],()=>s(87012));var l=o.O()}]);
